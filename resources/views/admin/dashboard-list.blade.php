fac9c9b09405c22e17bd36789c9c7f5a3160d04d
Muhammad Khan <muhammadkh4n@gmail.com>
2016-08-04 18:03:02 +0500

    University list table extra columns added

--- resources/views/admin/dashboard-list.blade.php
+++ resources/views/admin/dashboard-list.blade.php
@@ -13,14 +13,22 @@
       <tr>
         <th>id</th>
         <th>University Name</th>
+        <th>Address</th>
+        <th>Email</th>
         <th>Website</th>
+        <th>Est.</th>
+        <th>Pop (total, int, pk)</th>
         <th>Rank</th>
       </tr>
       @foreach($universities as $uni)
         <tr>
           <td>{{ $uni->id }}</td>
           <td>{{ $uni->name }}</td>
-          <td>{{ $uni->website }}</td>
+          <td>{{ $uni->address }}, {{ $uni->city }}, {{ $uni->country }}</td>
+          <td><a href="mailto:{{ $uni->email }}">{{ $uni->email }}</a></td>
+          <td><a href="{{ $uni->website }}">{{ $uni->website }}</a></td>
+          <td>{{ $uni->university_info->established }}</td>
+          <td>{{ $uni->university_info->population }}, {{ $uni->university_info->intpopulation }}, {{ $uni->university_info->pkpopulation }}</td>
           <td>{{ $uni->university_info->rank }}</td>
         </tr>
       @endforeach
